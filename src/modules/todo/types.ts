f4f9fecf6e951b9f16de2c4d5f73361089f9cb5d
LydiaYoon <narae456@gmail.com>
2022-08-12 17:59:28 +0900

    type 선언시 액션의 상위 name 추가 - redux 개발자 도구에서 action 확인시 구분하기 편하도록 before) SET_PAGE SET_HEADER_TITLE GET_TODOS GET_TODOS_SUCCESS
    after)
    layout/SET_PAGE
    layout/SET_HEADER_TITLE
    todo/GET_TODOS
    todo/GET_TODOS_SUCCESS

--- src/modules/todo/types.ts
+++ src/modules/todo/types.ts
@@ -1,21 +1,21 @@
 // type
 
-export const GET_TODOS = 'GET_TODOS';
-export const GET_TODOS_SUCCESS = 'GET_TODOS_SUCCESS';
-export const GET_TODOS_ERROR = 'GET_TODOS_ERROR';
+export const GET_TODOS = 'todo/GET_TODOS';
+export const GET_TODOS_SUCCESS = 'todo/GET_TODOS_SUCCESS';
+export const GET_TODOS_ERROR = 'todo/GET_TODOS_ERROR';
 
-export const GET_TODO_BY_ID = 'GET_TODO_BY_ID';
-export const GET_TODO_BY_ID_SUCCESS = 'GET_TODO_BY_ID_SUCCESS';
-export const GET_TODO_BY_ID_ERROR = 'GET_TODO_BY_ID_ERROR';
+export const GET_TODO_BY_ID = 'todo/GET_TODO_BY_ID';
+export const GET_TODO_BY_ID_SUCCESS = 'todo/GET_TODO_BY_ID_SUCCESS';
+export const GET_TODO_BY_ID_ERROR = 'todo/GET_TODO_BY_ID_ERROR';
 
-export const CREATE_TODO = 'CREATE_TODO';
-export const CREATE_TODO_SUCCESS = 'CREATE_TODO_SUCCESS';
-export const CREATE_TODO_ERROR = 'CREATE_TODO_ERROR';
+export const CREATE_TODO = 'todo/CREATE_TODO';
+export const CREATE_TODO_SUCCESS = 'todo/CREATE_TODO_SUCCESS';
+export const CREATE_TODO_ERROR = 'todo/CREATE_TODO_ERROR';
 
-export const UPDATE_TODO = 'UPDATE_TODO';
-export const UPDATE_TODO_SUCCESS = 'UPDATE_TODO_SUCCESS';
-export const UPDATE_TODO_ERROR = 'UPDATE_TODO_ERROR';
+export const UPDATE_TODO = 'todo/UPDATE_TODO';
+export const UPDATE_TODO_SUCCESS = 'todo/UPDATE_TODO_SUCCESS';
+export const UPDATE_TODO_ERROR = 'todo/UPDATE_TODO_ERROR';
 
-export const DELETE_TODO = 'DELETE_TODO';
-export const DELETE_TODO_SUCCESS = 'DELETE_TODO_SUCCESS';
-export const DELETE_TODO_ERROR = 'DELETE_TODO_ERROR';
+export const DELETE_TODO = 'todo/DELETE_TODO';
+export const DELETE_TODO_SUCCESS = 'todo/DELETE_TODO_SUCCESS';
+export const DELETE_TODO_ERROR = 'todo/DELETE_TODO_ERROR';
