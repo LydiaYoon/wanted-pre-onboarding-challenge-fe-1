433c235495a37adb6d3eea0075227e5dc5887bfd
LydiaYoon <narae456@gmail.com>
2022-08-19 02:50:30 +0900

    투두 리스트 할 일 목록 작성 후 추가(Create) 기능 구현

--- client/src/pages/Todos/TodoCreate/TodoCreate.style.tsx
+++ client/src/pages/Todos/TodoCreate/TodoCreate.style.tsx
@@ -11,21 +11,9 @@ export const TodoFormWrapper = styled.form`
   flex-direction: column;
   flex: 1;
 
-  input {
-    padding: 12px;
-    border-radius: 4px;
-    border: 1px solid #dee2e6;
-    font-size: 18px;
-    box-sizing: border-box;
-  }
-
   textarea {
-    padding: 12px;
-    border-radius: 4px;
-    border: 1px solid #dee2e6;
+    // border: 1px solid #dee2e6;
     height: 100%;
-    font-size: 18px;
-    box-sizing: border-box;
     resize: none;
   }
 `;
--- client/src/pages/Todos/TodoCreate/TodoCreate.tsx
+++ client/src/pages/Todos/TodoCreate/TodoCreate.tsx
@@ -1,31 +1,87 @@
-import React from 'react';
-import { MdCreate, MdCancel } from 'react-icons/md';
+import { useMutation } from '@tanstack/react-query';
+import { AxiosError } from 'axios';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { MdCancel, MdCreate } from 'react-icons/md';
+import { useSearchParams } from 'react-router-dom';
+import { ErrorResponse } from '../../../api/Api';
+import todoAPI, { TodoData, TodoParam, TodoResponse } from '../../../api/todo/todoApi';
 import { TodoCreateContainer, TodoFormWrapper } from './TodoCreate.style';
 
+type FormInputs = {
+  title: string;
+  content: string;
+};
+
 const TodoCreate = () => {
-  const handleSubmit = (event: React.MouseEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    const form = new FormData(event.currentTarget);
-    // TODO: 투두리스트 Create
-    // form.get('title');
-    // form.get('content');
+  const authToken = JSON.parse(localStorage.getItem('authToken') as string);
+  const [searchParams] = useSearchParams();
+  const todoId = searchParams.get('id');
+
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors, isValid, isDirty },
+  } = useForm<FormInputs>({
+    mode: 'onChange',
+  });
+
+  const onSubmit: SubmitHandler<FormInputs> = data => {
+    if (isValid && authToken.token) {
+      console.log(data);
+      console.log(authToken.token);
+      mutate({ ...data, authToken: authToken.token }, { onSuccess, onError });
+    }
   };
 
+  const onSuccess = (response: TodoResponse<TodoData>) => {
+    console.log(response);
+    //navigator('/');
+  };
+
+  const onError = ({ response }: AxiosError<ErrorResponse>) => {
+    console.log(response?.data.details);
+  };
+
+  const { mutate } = useMutation<TodoResponse<TodoData>, AxiosError<ErrorResponse>, TodoParam>(todoAPI.create);
+
   return (
-    <>
-      <TodoCreateContainer>
-        <TodoFormWrapper onSubmit={handleSubmit}>
-          <input name="title" type="text" placeholder="Title" autoFocus />
-          <textarea name="content" placeholder="Content" />
-          <button type="submit" className="success">
-            Submit <MdCreate />
-          </button>
-          <button type="reset" className="error">
-            Reset <MdCancel />
-          </button>
-        </TodoFormWrapper>
-      </TodoCreateContainer>
-    </>
+    <TodoCreateContainer>
+      <TodoFormWrapper onSubmit={handleSubmit(onSubmit)}>
+        <input
+          type="text"
+          {...register('title', {
+            required: 'Please write title',
+            minLength: {
+              value: 1,
+              message: 'Must write at least one letter.',
+            },
+          })}
+          autoFocus
+          placeholder="title"
+          className={errors.title?.message ? 'error' : ''}
+        />
+
+        <textarea
+          {...register('content', {
+            required: 'Please write content.',
+            minLength: {
+              value: 1,
+              message: 'Must write at least one letter.',
+            },
+          })}
+          placeholder="content"
+          className={errors.content?.message ? 'error' : ''}
+        />
+
+        <button type="submit" className="success" disabled={!isDirty && isValid}>
+          Submit <MdCreate />
+        </button>
+        <button type="reset" className="warning" onClick={() => reset()}>
+          Reset <MdCancel />
+        </button>
+      </TodoFormWrapper>
+    </TodoCreateContainer>
   );
 };
 
